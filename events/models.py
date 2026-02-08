33c9d64e6019ec1cc27626b4738c0a8353a32d0f
Refat123S <refatislamshammi444@gmail.com>
2026-02-09 00:46:31 +0600

    Updating events models and admin

--- events/models.py
+++ events/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
+from clubs.models import Club
 
-# Create your models here.
+class Event(models.Model):
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateTimeField()
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
