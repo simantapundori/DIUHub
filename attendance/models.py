54869cbcdbcf54150e7ec2093bedf6e5b0247cc9
Isratjahanifti <isratjahan64331@gmail.com>
2026-02-09 00:50:27 +0600

    Update attendance models and admin

--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from registrations.models import Registration
 
-# Create your models here.
+class Attendance(models.Model):
+    registration = models.ForeignKey(Registration, on_delete=models.CASCADE)
+    scanned_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Attendance {self.registration.id}"
