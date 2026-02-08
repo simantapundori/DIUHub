7a3f993243fbafcbcf23b94cd47ec0255ac22e4e
simantapundori <pundori2305341335@diu.edu.bd>
2026-02-09 00:38:31 +0600

    Update users/models.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('student', 'Student'),
+        ('admin', 'Club Admin'),
+        ('advisor', 'Faculty Advisor'),
+        ('committee', 'Committee Member'),
+        ('superadmin', 'Super Admin'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
