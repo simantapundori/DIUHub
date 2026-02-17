9573a7e675331b8191c31ebe3f9d624994bb9fbf
Refat123S <refatislamshammi444@gmail.com>
2026-02-18 00:30:17 +0600

    Registration Admin Updated

--- registrations/admin.py
+++ registrations/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Registration
 
-# Register your models here.
+
+@admin.register(Registration)
+class RegistrationAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'user',
+        'event',
+        'status',
+        'created_at',
+    )
+
+    list_filter = (
+        'status',
+        'event',
+        'created_at',
+    )
+
+    search_fields = (
+        'user__username',
+        'event__title',
+    )
+
+    ordering = (
+        '-created_at',
+    )
+
+    readonly_fields = (
+        'created_at',
+        'updated_at',
+    )
