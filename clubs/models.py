97ad9924ec9167d755a19ddbf2f9f34b2bf2e421
simantapundori <pundori2305341335@diu.edu.bd>
2026-02-17 14:40:07 +0600

    Club and Membership Model

--- clubs/models.py
+++ clubs/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Club(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Membership(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
+    payment_ref = models.CharField(max_length=100)
+    status = models.CharField(max_length=20, default='pending')
+
+    def __str__(self):
+        return f"{self.user.username} -> {self.club.name}"
