f77ce08f507e0d803535345c9c7d1a9758101069
Isrstjahanifti <isratjahan64331@gmail.com>
2026-02-18 13:46:30 +0600

    view.py added

--- attendance/views.py
+++ attendance/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from registrations.models import Registration
+from .models import Attendance
 
-# Create your views here.
+
+def mark_attendance(request, registration_id):
+
+    registration = Registration.objects.get(id=registration_id)
+
+    attendance, created = Attendance.objects.get_or_create(
+        registration=registration
+    )
+
+    attendance.attended = True
+    attendance.attended_at = timezone.now()
+
+    attendance.save()
+
+    return redirect('attendance_list')
+
+
+def attendance_list(request):
+
+    attendance = Attendance.objects.all()
+
+    return render(request, 'attendance/attendance_list.html', {
+        'attendance': attendance
+    })
