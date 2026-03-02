1c53ef9430b829e3f9709ac7174917369cf714b4
Refat123S <refatislamshammi444@gmail.com>
2026-03-02 22:23:36 +0600

    Registration view updated

--- registrations/views.py
+++ registrations/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+> **`registrations/views.py`**
+import qrcode
+from django.shortcuts import redirect
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from .models import Registration
+from events.models import Event
+import os
 
-# Create your views here.
+
+@login_required
+def register_event(request, event_id):
+
+    event = Event.objects.get(id=event_id)
+
+    registration, created = Registration.objects.get_or_create(
+        user=request.user,
+        event=event
+    )
+
+    if created:
+
+        qr_data = f"user:{request.user.id},event:{event.id}"
+
+        qr = qrcode.make(qr_data)
+
+        filename = f"qr_user{request.user.id}_event{event.id}.png"
+
+        filepath = os.path.join(settings.MEDIA_ROOT, 'qr_codes', filename)
+
+        os.makedirs(os.path.dirname(filepath), exist_ok=True)
+
+        qr.save(filepath)
+
+        registration.qr_code = f"qr_codes/{filename}"
+
+        registration.save()
+
+    return redirect('event_list')
