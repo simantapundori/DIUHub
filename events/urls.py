9b0a1b485a200187aa523f27a9f1baf5a638183f
simantapundori <pundori2305341335@diu.edu.bd>
2026-04-05 00:07:35 +0600

    Update urls.py

--- events/urls.py
+++ events/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.event_list, name='event_list'),
+    
+    path('register/<int:event_id>/', views.register_event, name='register_event'),
 ]
