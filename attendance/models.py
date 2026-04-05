0ffbd39c071a68f61e186a7fbc7091f931591182
Isrstjahanifti <isratjahan64331@gmail.com>
2026-04-06 01:13:56 +0600

    Attendance model updated

--- attendance/models.py
+++ attendance/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from registrations.models import Registration
 
+
 class Attendance(models.Model):
-    registration = models.ForeignKey(Registration, on_delete=models.CASCADE)
-    scanned_at = models.DateTimeField(auto_now_add=True)
+    registration = models.OneToOneField(Registration, on_delete=models.CASCADE)
+    attended = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Attendance {self.registration.id}"
+        return f"{self.registration.user.username} - {self.registration.event.title}"
